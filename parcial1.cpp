c0fb78d3525f64fcb2e181d70f7ae246cce817e4
arnisanchez02 <144382434+arnisanchez02@users.noreply.github.com>
2023-09-07 14:06:54 -0600

    Add files via upload

---
+++ parcial1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+	
+	int contador =0;
+	
+	printf( "\n  ");
+	
+	while (contador <= 9
+	{
+		printf( "%d ", contador );
+		contador ++;
+			
+	}
+	
+	
+	
+	
+	return 0;
+}
