034b72f8ba0c1310179acc39aa0299ac7f5e5dcd
arnisanchez02 <144382434+arnisanchez02@users.noreply.github.com>
2023-09-09 00:35:12 -0600

    Add files via upload

---
+++ ejercicio 2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+	
+	int suma;
+	int a=1;
+	int b=3;
+	suma=a+b;
+	
+	
+	printf("Si sumamos 1 + 3 la respueta es %i", suma);
+	
+	
+	return 0;
+}
+
+
